c791e840782cce361616ea3095cbebfc44d74045
Marcap00 <cappellatomarco@yahoo.com>
2024-11-11 12:51:39 +0100

    create

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,18 @@
+{{-- TITLE AUTHORE DESCRIPTION --}}
+
+<form action="">
+    <div class="mb-3">
+        <label for="author" class="form-label">Author:</label>
+        <input type="text" class="form-control" id="author" placeholder="Author">
+    </div>
+    <div class="mb-3">
+        <label for="title" class="form-label">Title:</label>
+        <input type="text" class="form-control" id="title" name="title" placeholder="Title">
+    </div>
+    <div class="mb-3">
+        <label for="description" class="form-label">Title:</label>
+        <input type="text" class="form-control" id="description" name="description" placeholder="Description">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="reset" class="btn btn-primary">Reset</button>
+</form>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,5 @@
 @section('head-title', 'Laravel Template')
 
 @section('main-content')
-<main>
-    <h1>Main</h1>
-</main>
+
 @endsection
